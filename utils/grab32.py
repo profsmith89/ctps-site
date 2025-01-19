8148030d5672f96c26d97f84761235c74eecaa33
Mike Smith <mike_smith@harvard.edu>
2025-01-19 12:47:57 -0500

    Extend grab32 to handle the grabbing of psets

--- utils/grab32.py
+++ utils/grab32.py
@@ -1,10 +1,13 @@
 ### utils/grab32.py
 """
-This script works within the cs50.dev world and grabs the CS32 GitHub
-repo files for a book chapter or a programming problem set.
+This script works within the cs50.dev world and grabs three kinds
+of files:
+  1. the CS32 GitHub repo files for a book chapter;
+  2. the .devcontainer.json file for configuring the codespace;
+  3. the files for a programming problem set kept in cs32-public.
 
-For example, if you wanted the book files for Chapter 4, you'd run
-this script as follows:
+If you wanted the book files for Chapter 4, you'd run this script
+as follows:
 
 $ python3 grab32.py chap04
 
@@ -35,6 +38,22 @@ This script does this setup work if you run it with this special parameter:
 
 $ python3 grab32.py cs32-setup
 
+Finally, it can grab CS32 pset directories that we've hidden in the
+cs32-public Google drive. To grab these zipped directories, you specify
+the pset you want:
+
+$ python3 grab32.py pset1
+
+The input parameter (i.e., `pset1` in this example) should match the
+name of a zip file in cs32-public.  The script will wget the zip file
+using the proper incantation for grabbing files from Google Drive,
+unzip the file, and remove the downloaded zip file.
+
+If a subdirectory of the same name already exists in the current
+codespace, this script assumes that the user wants a new clean copy
+of the pset's files.  It will name the clean copy `PSET_clean` (e.g.,
+`pset1_clean`).
+
 Author: Mike Smith (with reference help from GPT-4o)
 Date: January 2025
 """
@@ -51,6 +70,17 @@ ORG_URL = 'https://github.com/seas-cs32/'
 MAIN_ZIP_PATH = '/archive/refs/heads/main.zip'
 CODESPACES_ROOT = '/workspaces'
 SETUP_REPO = 'template'
+
+# FILE_IDs of the pset zip files in cs32-public. The FILE_ID of
+# a shareable Google Drive URL is here:
+# https://drive.google.com/file/d/FILE_ID/view?usp=sharing
+PSETS = {
+    'pset1': '1CQWXhfdR5TYECfWRRnXzDEJxhEzdqx4m',
+    'pset2': '1XBnslGVWqlVhSjBFMs63LDuxz-t3cfT2',
+    'pset3': '17SEp67vLgMytPuyuHNNnZxF5pPRYctVV',
+    'pset4': '1-oOHOW9m7RGyuVUmRM-LyLrFnDUtZEEM',
+    'pset5': '1jYYeNKa6fAdtnQ4kiIUSCj-j8U7SLSmP',
+}
 
 
 def determine_working_dir():
@@ -112,14 +142,21 @@ def main():
 
     repo = sys.argv[1]
 
+    # Defaults to chap grabs
+    SETUP = False
+    PSET = False
+
     # Check validity of input parameter
     if repo == 'cs32-setup':
         # Special case
         SETUP = True
         repo = SETUP_REPO
+    elif repo[:len('pset')] == 'pset':
+        PSET = True
+        # Check for a valid pset number
+        if not re.fullmatch(r'pset[1-5]', repo):
+            sys.exit(f"ERROR: {repo} is not a valid; did you mistype it?")
     else:
-        SETUP = False
-
         # Check for and gracefully handle bad parameters
         if re.fullmatch(r'chap[01][1-8]', repo) or repo == 'chap09' or repo == 'chap10':
             pass
@@ -133,13 +170,40 @@ def main():
     codespace_path = determine_working_dir()
     print(f"... Working in directory: {codespace_path}")
 
-    # Create URL to a zipfile of the specified github repo
-    url = ORG_URL + repo + MAIN_ZIP_PATH
-    zip_fname = os.path.basename(url)
+    # Create URL to ...
+    if PSET:
+        # ... a zipfile in cs32-public
+        url = f'https://drive.google.com/uc?export=download&id={PSETS[repo]}'
+        zip_fname = f'{repo}.zip'
+    else:
+        # Create URL to a zipfile of the specified github repo
+        url = ORG_URL + repo + MAIN_ZIP_PATH
+        zip_fname = os.path.basename(url)
+
+    if PSET:
+        # Unzip in a temp directory since no -main on the zipped directory
+        TMP_DIR = '.tmp_cs32'
+
+        # Create the temp directory
+        try:
+            os.makedirs(TMP_DIR, exist_ok=True)
+            print(f"... directory '{TMP_DIR}' created successfully")
+        except OSError as error:
+            sys.exit(f"ERROR creating directory {TMP_DIR}: {error}")
+
+        # Jump into the temp directory for the unzip
+        try:
+            os.chdir(TMP_DIR)
+            print(f"... jumped into '{TMP_DIR}'")
+        except Exception as e:
+            sys.exit(f"ERROR changing into directory {TMP_DIR}: {e}")
 
     # Download the repo's files (quietly)
     try:
-        command = ['wget', url]
+        if PSET:
+            command = ['wget', '--no-check-certificate', url, '-O', zip_fname]
+        else:
+            command = ['wget', url]
         subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
     except Exception as e:
         sys.exit(f"ERROR executing wget command: {e}")
@@ -164,17 +228,35 @@ def main():
         sys.exit(f"ERROR removing file {zip_fname}: {e}")
     print(f"... Removed {zip_fname}")
 
-    # Rename the repo directory to remove the "-main" suffix,
-    # but how we do this depends on whether the user is downloading
-    # the repo for the first time or as another clean copy.
-    if os.path.exists(repo):
-        # Rename as a clean copy of repo
-        my_rename(repo + '-main', repo + '_clean')
-        print(f"... Renamed {repo}-main to {repo}_clean")
+    if PSET:
+        if os.path.exists(f'../{repo}'):
+            # Rename as a clean copy of repo
+            my_rename(repo, f'../{repo}_clean')
+            print(f"... Renamed {TMP_DIR}/{repo} to {repo}_clean")
+        else:
+            # Straightforward rename
+            my_rename(repo, f'../{repo}')
+            print(f"... Renamed {TMP_DIR}/{repo} to {repo}")
+
+        # Jump back to the user's home directory
+        try:
+            os.chdir('..')
+            print(f"... jumped back into home directory")
+        except Exception as e:
+            sys.exit(f"ERROR changing into directory '..': {e}")
+
     else:
-        # Straightforward rename
-        my_rename(repo + '-main', repo)
-        print(f"... Renamed {repo}-main to {repo}")
+        # Rename the repo directory to remove the "-main" suffix,
+        # but how we do this depends on whether the user is downloading
+        # the repo for the first time or as another clean copy.
+        if os.path.exists(repo):
+            # Rename as a clean copy of repo
+            my_rename(repo + '-main', repo + '_clean')
+            print(f"... Renamed {repo}-main to {repo}_clean")
+        else:
+            # Straightforward rename
+            my_rename(repo + '-main', repo)
+            print(f"... Renamed {repo}-main to {repo}")
 
     # Clean up if running cs32-setup
     if SETUP:
